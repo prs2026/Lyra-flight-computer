ed938b40de7a11b2a356c895942bb267009f2e9b
Polaris Bear <39930563+prs2026@users.noreply.github.com>
2024-01-06 02:48:09 -0800

    add lib

---
+++ Software/Ground/lero reciver/lib/macros/macros.h
@@ -0,0 +1,24 @@
+#if !defined(MACROS)
+#define MACROS
+#include <Arduino.h>
+
+#define BUTTON1 7
+#define BUTTON2 12
+#define BUTTON3 8
+#define BUTTON4 6
+#define BUTTON5 9
+
+#define LRC 15
+#define BCLK 14
+#define DIN 13
+
+#define MISOSD 16
+#define SCKSD 18
+#define MOSISD 19
+#define CS_SD 18
+
+#define LCDDI 22 // also cs
+#define LCDRW 11 // also mosi
+#define LCDE 10 // also sck
+
+#endif // MACROS
--- Software/Ground/lero reciver/src/main.cpp
+++ Software/Ground/lero reciver/src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
+#include <macros.h>
+#include <U8g2lib.h>
 
+U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ LCDE, /* data=*/ LCDRW, /* CS=*/ LCDDI);
 
 void setup()
 {
