708afdf6d7b62a74b24c6ae572a43217055fb14e
Polaris Bear <39930563+prs2026@users.noreply.github.com>
2025-02-11 07:54:28 -0800

    hope initng

--- Hope/Firmware/Testing/blink_simple/blink_simple.c
+++ Hope/Firmware/Testing/blink_simple/blink_simple.c
@@ -5,26 +5,26 @@
  */
 
 #include "pico/stdlib.h"
-
+#define LEDPIN 8
 #ifndef LED_DELAY_MS
 #define LED_DELAY_MS 500
 #endif
 
 // Initialize the GPIO for the LED
 void pico_led_init(void) {
-#ifdef PICO_DEFAULT_LED_PIN
+#ifdef LEDPIN
     // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
     // so we can use normal GPIO functionality to turn the led on and off
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_init(LEDPIN);
+    gpio_set_dir(LEDPIN, GPIO_OUT);
 #endif
 }
 
 // Turn the LED on or off
 void pico_set_led(bool led_on) {
-#if defined(PICO_DEFAULT_LED_PIN)
+#if defined(LEDPIN)
     // Just set the GPIO on or off
-    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+    gpio_put(LEDPIN, led_on);
 #endif
 }
 
